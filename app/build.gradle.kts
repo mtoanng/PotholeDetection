1922b4522bf04205e1de6d83927a4a10657ed15d
YouQuinney <myquyen.19092003@gmail.com>
2024-12-03 14:18:30 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,22 @@ android {
     namespace = "com.example.myapplication"
     compileSdk = 34
 
+    buildFeatures{
+        viewBinding = true
+    }
+
+    packaging {
+        dex {
+            useLegacyPackaging = false
+        }
+        resources {
+            excludes.add("META-INF/DEPENDENCIES")
+            excludes.add("META-INF/LICENSE")
+            excludes.add("META-INF/NOTICE")
+            excludes.add("mozilla/public-suffix-list.txt")
+        }
+    }
+
     defaultConfig {
         applicationId = "com.example.myapplication"
         minSdk = 34
@@ -44,11 +60,18 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    implementation ("com.airbnb.android:lottie:6.0.0")
-//    implementation ("com.etebarian:meow-bottom-navigation:1.2.0")
-    implementation ("org.projectlombok:lombok:1.18.28")
-    annotationProcessor ("org.projectlombok:lombok:1.18.28")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.airbnb.android:lottie:6.0.0")
+    implementation("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+    implementation ("com.google.android.gms:play-services-auth:21.2.0")
+    implementation("com.google.oauth-client:google-oauth-client:1.34.1")
+    implementation("com.google.apis:google-api-services-oauth2:v2-rev157-1.25.0")
+    implementation("com.google.oauth-client:google-oauth-client-java6:1.34.1")
+    implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
+    implementation ("com.google.android.gms:play-services-auth:21.2.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+    implementation("com.google.firebase:firebase-auth")
 
 
 }
